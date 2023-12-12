49235c7042c8e1b20ca55aacc4f2b9a98219fd52
Andrei Rybnov <rybnov@dws.sh>
2023-12-12 12:51:52 +0400

    Feat: DWS-1315 update path

--- internal/types/const.go
+++ internal/types/const.go
@@ -1,7 +1,7 @@
 package types
 
 const (
-	APIURL                = "https://app.dws.sh"
+	APIURL                = "https://app.nodeshift.com"
 	DefaultTimeoutSeconds = 120
 	GPUEndpoint           = "/api/terraform/gpu"
 )
